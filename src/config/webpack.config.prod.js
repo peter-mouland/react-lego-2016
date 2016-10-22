55954e9f86d0c7df055d52dce15d66ffb8b2be8d
Peter Mouland <peter.mouland@gmail.com>
2016-10-22 12:48:31 +0100

    Swap webpack v1 for v2

--- src/config/webpack.config.prod.js
+++ src/config/webpack.config.prod.js
@@ -1,11 +1,5 @@
-const webpack = require('webpack');
-
 const { SRC } = require('./paths');
 const defaultConfig = require('./webpack.common');
-
-defaultConfig.plugins.concat([
-  new webpack.optimize.UglifyJsPlugin({ minimize: true })
-]);
 
 const prodConfig = Object.assign({}, defaultConfig, {
   entry: { app: [`${SRC}/styles/app.scss`, `${SRC}/client-entry.js`] }
