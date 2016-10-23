099068d59054e38625ce5f178364efa2492127cf
Peter Mouland <peter.mouland@gmail.com>
2016-10-23 15:30:43 +0100

    add React-hot-loader v3 to Koa

--- src/config/webpack.config.dev.js
+++ src/config/webpack.config.dev.js
@@ -1,9 +1,21 @@
+const webpack = require('webpack');
+
 require('./environment');
 const { SRC } = require('./paths');
 const defaultConfig = require('./webpack.common');
 
 const devConfig = Object.assign({}, defaultConfig, {
-  entry: { app: [`${SRC}/styles/app.scss`, `${SRC}/client-entry.js`] }
+  entry: {
+    app: [
+      'react-hot-loader/patch',
+      'webpack-hot-middleware/client',
+      `${SRC}/styles/app.scss`,
+      `${SRC}/client-entry.js`
+    ]
+  },
 });
 
+devConfig.plugins.push(
+  new webpack.HotModuleReplacementPlugin()
+);
 module.exports = devConfig;
